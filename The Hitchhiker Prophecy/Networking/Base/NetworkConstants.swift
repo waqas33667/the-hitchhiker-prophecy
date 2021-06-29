100c96824821e10ccbbeeaefe9d8e3d5efda2bc2
waqas33667 <43587289+waqas33667@users.noreply.github.com>
2021-06-29 11:30:04 +0500

    Add files via upload
    Initial Code

---
+++ The Hitchhiker Prophecy/Networking/Base/NetworkConstants.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkConstants.swift
+//  The Hitchhiker Prophecy
+//
+//  Created by Mohamed Matloub on 6/10/20.
+//  Copyright © 2020 SWVL. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkConstants {
+    static let baseUrl = "https://gateway.marvel.com"
+    static let publicKey = "" // TODO: Add your marvel keys
+    static let privateKey = "" // TODO: Add your marvel keys
+}
